f1537fa768bac965e8e223919dc7b788698e90de
Martin J Page <martinjpage@outlook.com>
2020-09-17 15:18:41 +0200

    Changed format so that file paths must be specified before the function run

--- code/main.py
+++ code/main.py
@@ -3,11 +3,19 @@ from data_formatting import data_form
 from models import get_classification_model
 from export import zindi_submission, prepare_for_submission 
 
+#set file paths
+#train_path = "/content/drive/My Drive/Regression Challenge Shared Folder/Data/Train.csv"
+#test_path = "/content/drive/My Drive/Regression Challenge Shared Folder/Data/Test.csv"
+output_path = "/content/drive/My Drive/Regression Challenge Shared Folder/Outputs/log_reg_results.csv"
+
+train_path = "DSIregression/Data/Train.csv"
+test_path = "DSIregression/Data/Test.csv"
+output_path = "DSIregression/Outputs/results/csv"
 
 def run():
 	# Import the Datasets
-	train_data = pd.read_csv("/content/drive/My Drive/Regression Challenge Shared Folder/Data/Train.csv", index_col="ID") 
-	test_data = pd.read_csv("/content/drive/My Drive/Regression Challenge Shared Folder/Data/Test.csv", index_col="ID")
+	train_data = pd.read_csv(train_path, index_col="ID")
+	test_data = pd.read_csv(test_path, index_col="ID")
 
 	# Data formatting
 	X_train, y_train, X_test, y_test, cat_cols, num_cols = data_form(train_data, test_data)
@@ -19,7 +27,6 @@ def run():
 	# Predict
 	y_pred = model.predict(X_test)
 	y_pred = prepare_for_submission(y_pred, y_test)
-	output_path = "/content/drive/My Drive/Regression Challenge Shared Folder/Outputs/log_reg_results.csv"
 
 	# Export to csv file
 	zindi_submission(y_pred, output_path)
