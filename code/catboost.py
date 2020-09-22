fee48b066ad8b942ef0c7ed3b6ef147016e40370
ncarraz <40558271+ncarraz@users.noreply.github.com>
2020-09-22 10:05:06 +0200

    add data formatting

--- code/catboost.py
+++ code/catboost.py
@@ -1,6 +1,15 @@
 from catboost import CatBoostClassifier
 from export import zindi_submission
+from data_formatting import simple_data_form
 
+train_path = "DSIregression/Data/Train.csv"
+test_path = "DSIregression/Data/Test.csv"
+output_path = "DSIregression/Outputs/catboost.csv"
+
+train_data = pd.read_csv(train_path, index_col="ID")
+test_data = pd.read_csv(test_path, index_col="ID")
+
+X_train, y_train, X_test = simple_data_form(train_data, test_data)
 
 model = CatBoostClassifier(task_type="GPU")
 model.fit(X_train, y_train, cat_features=categorical_col)
@@ -16,4 +25,4 @@ y_pred[X_test_product == 1] = 1 # set 1 where it was already 1
 y_pred
 
 # Export to csv for  submission
-zindi_submission(y_pred, ""DSIregression/Outputs/catboost.csv")
+zindi_submission(y_pred, output_path)
